a216eabe8f0ef0e8e5ec93d1d4f0dd6f75ea5d28
adityasimant <adityasimant88@gmail.com>
2023-08-13 17:59:09 +0530

    Installed and configured React-Bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// React bootstrap
+import "../node_modules/react-bootstrap/dist/react-bootstrap"
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
