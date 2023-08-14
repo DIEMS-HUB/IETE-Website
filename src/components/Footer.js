90a0f0701e462ab13077b4d458c5aae7c8f909a8
adityasimant <adityasimant88@gmail.com>
2023-08-14 19:29:28 +0530

    added component footer

---
+++ src/components/Footer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                Code for footer comes here!
+            </div>
+        );
+    }
+}
+
+export default Footer;
