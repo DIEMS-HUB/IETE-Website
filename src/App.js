2cd872eda63aba381c3b0ea68b89b6372eb9eda5
Akshayp02 <peherkarakshay3092@gmail.com>
2023-08-15 18:07:07 +0530

    done with #6

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Events from './components/Events';
 import CoreTeam from './components/CoreTeam';
 import Contact from './components/Contact';
 import Home from './components/Home';
+import Footer from './components/Footer';
 function App() {
   return (
     <>
@@ -21,7 +22,9 @@ function App() {
           <Route path="/coreteam" element={<CoreTeam />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="*" element={<Navigate to="/"/>} />
+
         </Routes>
+        
         </Router>
     </>
   );
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,77 @@
 import React, { Component } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import './Footer.css'; // Import your custom CSS
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebook, faInstagram, faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
 
 class Footer extends Component {
     render() {
         return (
-            <div>
-                Code for footer comes here!
+            <div className="page-content">
+            <Container>
+                <Row>
+                    {/* Your content */}
+                </Row>
+            </Container>
+            <footer className="footer">
+            <Row>
+                        <Col className="text-center">
+                            <p>Made with ❤ IETE DIEMS.</p>
+                        </Col>
+                        </Row>
+                <Container>
+                    
+                    <Row className="footer-content">
+                    
+                        <Col xs={12} md={3} className="text-center">
+                            <h5>Coming Soon On</h5>
+                            <ul className="list-unstyled d-flex social-icons">
+                                <li>
+                                    <a href="https://www.instagram.com/your_instagram_username">
+                                        <FontAwesomeIcon icon={faInstagram} />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://www.linkedin.com/in/your_linkedin_profile">
+                                        <FontAwesomeIcon icon={faLinkedin} />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://twitter.com/your_twitter_username">
+                                        <FontAwesomeIcon icon={faTwitter} />
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://Facebook.com/">
+                                        <FontAwesomeIcon icon={faFacebook} />
+                                    </a>
+                                </li>
+                            </ul>
+                        </Col>
+                       
+                        <Col xs={12} md={3} className="text-right">
+                            <h5>Contact Details</h5>
+                            <ul className="list-unstyled d-flex social-icons">
+                               <p>iete@dietms.org<br/>
+                                Deogiri Institute of Engineering and Management Studies,
+                                Railway Station Road, Chh. Sambhajinagar – 431005.
+                               </p>
+                            </ul>
+                        </Col>
+                    </Row>
+                    <Row>
+                        <Col className="text-center">
+                            <p>&copy; {new Date().getFullYear()} IETE DIEMS ISF. All rights reserved.</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </footer>
             </div>
         );
     }
 }
 
+library.add(fab);
 export default Footer;
