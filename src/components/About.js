3c822fe9302290a5e65fae5a701db093ce00af86
adityasimant <adityasimant88@gmail.com>
2023-08-15 15:25:08 +0530

    LAnding page

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                this is about page
+            </div>
+        );
+    }
+}
+
+export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react';
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import homeLogo from "../../src/assets/websitelogo.png";
+import Type from "./Type";
+import Particle from "./Particles";
 
-class Home extends Component {
-    render() {
-        return (
-            <div>
-                This is home page
-            </div>
-        );
-    }
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+      <Particle />
+        <Container className="home-content">
+          <Row>
+        
+            <Col >
+              <img
+                src={homeLogo}
+                alt="home pic"
+                style={{ maxHeight: "450px" }}
+              />
+            </Col>
+            <Col md={7} className="home-header">
+              
+              <h1 className="heading-name">
+                Welcome to 
+                <strong className="main-name"> Institute of Electronics and Telecommunication Engineering DIEMS  ISF</strong>
+              </h1>
+
+              <div style={{ padding: 50, textAlign: "left" }}>
+                <Type />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </section>
+  );
 }
 
 export default Home;
