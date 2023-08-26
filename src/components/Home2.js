43e4c47f2cd22ca4bfa2d2a58d8dac3b86451889
Akshayp02 <peherkarakshay3092@gmail.com>
2023-08-26 21:35:34 +0530

    Merge branch 'main' of https://github.com/DIEMS-HUB/IETE-Website

--- src/components/Home2.js
+++ src/components/Home2.js
@@ -8,6 +8,7 @@ import {
 import { FaLinkedinIn } from "react-icons/fa";
 import About from "./About";
 import Faculty from "./Faculty";
+import wrldmap from "../assets/undraw_connected_world_wuay.svg"
 
 function Home2() {
   return (
@@ -18,7 +19,16 @@ function Home2() {
             <h1 style={{ fontSize: "2.6em" }}>
               What is  <span className="purple"><b>IETE</b>  </span> Student Forum ?
             </h1>
-            <p className="home-about-body" style={{ textAlign: "justify" }}>
+            <img
+                    src={wrldmap}
+                    alt="world map"
+                    style={{
+                        maxHeight: "200px", marginTop: "70px", marginBottom: "50px", display: "block",
+                        marginLeft: "auto",
+                        marginRight: "auto"
+                    }}
+                />
+            <p className="home-about-body" style={{ textAlign: "justify"}}>
 
               <br />
               IETE ISF is a student-driven initiative affiliated with the Institution of Electronics and Telecommunication Engineers (IETE). It aims to empower students by fostering collaborations with leading industry players, organizing industry-centric events, workshops, seminars, and networking opportunities. By bringing students and industry experts together, IETE ISF facilitates knowledge exchange, career development, and innovation in line with the latest industry trends and practices.
