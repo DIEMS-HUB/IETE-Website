e5e0873c9b22cbf8a3e3c92319d0bb9788a4bc5c
adityasimant <adityasimant88@gmail.com>
2023-10-19 21:16:26 +0530

    Some changes

--- src/components/Events.js
+++ src/components/Events.js
@@ -1,26 +1,45 @@
 import React, { Component } from 'react';
-import { Container} from 'react-bootstrap';
-import underconst from "../assets/undraw_under_construction_-46-pa.svg"
+import { Container, Row, Col } from 'react-bootstrap';
+// import underconst from "../assets/undraw_under_construction_-46-pa.svg"
+import Card from 'react-bootstrap/Card';
+import inaugration from '../assets/Inaugration.png'
+import Badge from 'react-bootstrap/Badge';
+
 
 class Events extends Component {
     render() {
         return (
-
-
-            <Container fluid className="project-section">
+            <Container fluid className="event-section" >
                 <h1 className="project-heading" style={{ textAlign: "center" }}>
                     Stay tuned for upcoming <span className='purple'>events</span> !
                 </h1>
-                <img
-                    src={underconst}
-                    alt="Nothing to show here"
-                    className="img-fluid"
-                    style={{
-                        maxHeight: "332px", marginTop: "70px", marginBottom: "50px", display: "block",
-                        marginLeft: "auto",
-                        marginRight: "auto"
-                    }}
-                />
+
+                <Row className='event-row'>
+                    <Col md={8}>
+                        <Card className='event-card'>
+                            <Card.Body style={{ padding: "2rem" }}>
+
+                                <Card.Title><h2> A New Start! </h2><Badge bg="danger" >Upcoming</Badge>
+                                   
+
+                                </Card.Title>
+                                <Card.Text>
+                                    You're cordially invited to the IETE Forum Inauguration at Deogiri Institute of Engineering and Management Studies. Join us on 25th Oct 2024 to celebrate the launch of this vibrant platform for electronics and telecommunications enthusiasts. Expect insightful speakers, networking opportunities, and a journey into the future of technology. Don't miss this exciting event!
+                                </Card.Text>
+
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <img
+                            src={inaugration}
+                            alt="home pic"
+                            style={{ maxHeight: "250px" }}
+                            className="img-fluid"
+                        />
+                    </Col>
+                </Row>
+
             </Container>
         );
     }
