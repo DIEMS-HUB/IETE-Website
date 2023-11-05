af64624b94df7a6bfc19421ff0a94c9fb9802db8
adityasimant <adityasimant88@gmail.com>
2023-11-05 20:51:18 +0530

    update

--- src/components/ManagementTeam.js
+++ src/components/ManagementTeam.js
@@ -5,7 +5,7 @@ import Table from 'react-bootstrap/Table';
 class ManagementTeam extends Component {
     render() {
         return (
-            <Table striped bordered hover>
+            <Table striped bordered hover className='associate-table'>
                                 <thead>
                                     <tr>
                                         <th>#</th>
@@ -13,57 +13,58 @@ class ManagementTeam extends Component {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    <tr>
+                                <tr>
                                         <td>1</td>
-                                        <td>Parth Shahane</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2</td>
-                                        <td>Prathamesh Waykos</td>
-                                    </tr>
-                                    <tr>
-                                        <td>3</td>
-                                        <td>Dhanshree Mahajan</td>
-                                        
-                                    </tr>
-                                    <tr>
-                                        <td>4</td>
-                                        <td>Akansha Bari</td>
-                                        
-                                    </tr>
-                                    <tr>
-                                        <td>5</td>
-                                        <td>Sakshi Dane</td>
-                                        
-                                    </tr>
-                                    <tr>
-                                        <td>6</td>
-                                        <td>Sanika Gorade</td>
-                                        
-                                    </tr>
-                                    <tr>
-                                        <td>7</td>
-                                        <td>Sanjana Patil</td>
-                                        
-                                    </tr>
-                                    <tr>
-                                        <td>8</td>
-                                        <td>Nakshatra Chikhalikar</td>
-                                        
-                                    </tr>
-                                    <tr>
-                                        <td>9</td>
                                         <td>Aditi Mulkewar</td>
                                         
                                     </tr>
                                     <tr>
+                                        <td>2</td>
+                                        <td>Akansha Bari</td>
+                                        
+                                    </tr>
+                                    <tr>
+                                        <td>3</td>
+                                        <td>Anushka Jain</td>
+                                        
+                                    </tr>
+                                    <tr>
+                                        <td>4</td>
+                                        <td>Dhanshree Mahajan</td>
+                                        
+                                    </tr>
+                                    <tr>
+                                        <td>5</td>
+                                        <td>Nakshatra Chikhalikar</td>
+                                        
+                                    </tr>
+                                    <tr>
+                                        <td>6</td>
+                                        <td>Parth Shahane</td>
+                                    </tr>
+                                    <tr>
+                                        <td>7</td>
+                                        <td>Prathamesh Waykos</td>
+                                    </tr>
+                                    <tr>
+                                        <td>8</td>
+                                        <td>Punam Garud</td>
+                                        
+                                    </tr>
+                                   
+                                    <tr>
+                                        <td>9</td>
+                                        <td>Sakshi Dane</td>
+                                        
+                                    </tr>
+                                    <tr>
                                         <td>10</td>
-                                        <td>Vaishali Khandekar</td>
+                                        <td>Sanika Gorade</td>
                                         
                                     </tr>
                                     <tr>
                                         <td>11</td>
-                                        <td>Vidhi Agrawal</td>
+                                        <td>Sanjana Patil</td>
                                         
                                     </tr>
                                     <tr>
@@ -71,16 +72,21 @@ class ManagementTeam extends Component {
                                         <td>Sanjana Mahajan</td>
                                         
                                     </tr>
+                                    
+                                    
                                     <tr>
                                         <td>13</td>
-                                        <td>Anushka Jain</td>
+                                        <td>Vaishali Khandekar</td>
                                         
                                     </tr>
                                     <tr>
                                         <td>14</td>
-                                        <td>Punam Garud</td>
+                                        <td>Vidhi Agrawal</td>
                                         
                                     </tr>
+                                    
+                                    
+                                    
                                     <tr>
                                         <td>15</td>
                                         <td>Vishal Khairnar</td>
